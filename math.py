39b75f5853f55fcbd7f5618e92c64eb520032f0f
Thomas-Faure <thomas.faure@etu.univ-amu.fr>
2017-10-26 09:03:30 +0200

    version combinée

--- math.py
+++ math.py
@@ -3,9 +3,8 @@ import random
 import time
 import matplotlib.pyplot as plt
 
-
 def draw(mat):
-    global canvas
+    global grille
     size = len(mat)
     width = 600/size
     for i in range(size):
@@ -16,14 +15,31 @@ def draw(mat):
                 color = 'blue'
             else:
                 color = 'white'
-            cell = canvas.create_rectangle(i*width, j*width, (i+1)*width, (j+1)*width, fill=color)
+            cell = grille.create_rectangle(i*width, j*width, (i+1)*width, (j+1)*width, fill=color)
+
+            
+
+def tracer(liste, size, population):
+    global graph, line_r, line_b
+    top = graph.create_line(0, 500-(size*size*2), 600, 500-(size*size*2), fill='black')
+    bottom = graph.create_line(0, 500, 600, 500, fill='black')
+    curve = [0,500]
+    for i in range(len(liste)):
+        curve.append(int(600/(len(liste))*(i+1)))
+        curve.append(500-int(liste[i])*2)
+    if population == 'red':
+        graph.delete(line_r)
+        line_r = graph.create_line(curve, fill='red')
+    if population == 'blue':
+        graph.delete(line_b)
+        line_b = graph.create_line(curve, fill='blue')
 
 def force(mat, x, y):
     cpt = 0
     for i in range(-1,2):
         for j in range(-1,2):
             if x+i>=0 and x+i<len(mat) and y+j>=0 and y+j<len(mat) and (i!=0 or j!=0):
-                #si le voisin est dans la matrice  et pas lui mÃªme
+                #si le voisin est dans la matrice  et pas lui même
                 cpt += mat[x+i][y+j]
     return cpt
 
@@ -48,7 +64,7 @@ def step(mat):
     for x in range(len(mat)):
         next_row = []
         for y in range(len(mat)):
-            print(force(mat, x, y))
+            #print(force(mat, x, y))
             if force(mat, x, y) > 0:
                 next_row.append(random.randint(1,11))
             elif force(mat, x, y) < 0:                
@@ -58,26 +74,14 @@ def step(mat):
         next_mat.append(next_row)
     return next_mat
 
-def compter(mat):
+def compter(mat, population):
     compteur = 0
     for row in mat:
         for column in row:
-            if column > 0:
-                compteur += 1
-    return compteur
-
-def compter2(mat):
-    compteur = 0
-    for row in mat:
-        for column in row:
-            if column < 0:
-                compteur += 1
-    return compteur
-def compter3(mat):
-    compteur = 0
-    for row in mat:
-        for column in row:
-            if column == 0:
+            if population == 0:
+                if column == 0:
+                    compteur += 1
+            elif column*population > 0:
                 compteur += 1
     return compteur
 
@@ -85,26 +89,32 @@ size = 13
 mat = generate_matrix(size)
 fen = Tk()
 fen.wm_attributes("-topmost", 1)
-global canvas
-canvas = Canvas(fen, width=600, height=600, background='white')
-canvas.pack()
-#label=Label(fen,text=str(compteur), fg="black",width=31,font="parade 42 bold")
-#label.pack()
-op = False
+global grille, graph
+grille = Canvas(fen, width=600, height=600, background='white')
+grille.pack(side=LEFT)
+graph = Canvas(fen, width=600, height=600, background='white')
+graph.pack(side=RIGHT)
+liste_rouge = []
+liste_bleu = []
+global line_r, line_b
+line_r = graph.create_line(0,0,0,0)
+line_b = graph.create_line(0,0,0,0)
+#Label(fen,text=str(compteur), fg="black",width=31,font="parade 42 bold")
 liste= []
 liste2= []
 liste3= []
-for loop in range(30):
-    #print("rouges: " + str(compter(mat)) + " | " + str(compter(mat)/(len(mat)*len(mat))*100) + "%")
-    liste.append(compter(mat))
-    liste2.append(compter2(mat))
-    liste3.append(compter3(mat))
+for loop in range(100):
     draw(mat)
-    if op == False :
-        op = True
+    liste.append(compter(mat,+1))
+    liste2.append(compter(mat,-1))
+    liste3.append(compter(mat,0))
+    liste_rouge.append(compter(mat, +1))
+    tracer(liste_rouge, size, 'red')
+    liste_bleu.append(compter(mat, -1))
+    tracer(liste_bleu, size, 'blue')
     fen.update()
     mat=step(mat)
-fen.mainloop()
+
 plt.plot(liste)
 plt.plot(liste2)
 plt.plot(liste3)
@@ -112,5 +122,5 @@ plt.ylabel('nombre de rouge')
 plt.xlabel('temps')
 plt.show()
 
-    
+fen.mainloop()    
 
